4070251fce120d0504f4eb7dbafacdf2b464f244
kevan14 <genger-man@hotmail.com>
2018-04-18 11:40:21 +0200

    Added project and created models for personas

---
+++ OurWebsite/OurWebsite/Controllers/PersonasController.cs
@@ -0,0 +1,28 @@
+﻿using OurWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurWebsite.Controllers
+{
+    public class PersonasController : Controller
+    {
+        // GET: Personas/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Us()
+        {
+            IList<Persona> personas = new List<Persona>(); 
+            Persona emil = new Persona() { Education = "Software Engineering", Age = 23, Description = "Hello there, I like to party and make smooth tender love with my wife.", Name = "Emil" };
+            Persona kennet = new Persona() { Education = "Software Engineering", Age = 25, Description = "Hello there, I like to shit and EAT all day long.", Name = "Kennet" };
+            personas.Add(emil);
+            personas.Add(kennet);
+            return View(personas);
+        }
+    }
+}
---
+++ OurWebsite/OurWebsite/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OurWebsite.Models
+{
+    public class Persona
+    {
+        public String Education { get; set; }
+        public int Age { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+    }
+}
