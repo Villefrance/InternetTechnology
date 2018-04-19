bbfbc970f395690687f87044d963181b4fed5bdc
kevan14 <genger-man@hotmail.com>
2018-04-19 10:35:12 +0200

    Signup form works

---
+++ OurWebsite/OurWebsite/Forms/SignupForm.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SignupForm.aspx.cs" Inherits="OurWebsite.Forms.SignupForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="SignupForm" runat="server">
+        <div>
+            <p>Name: </p> <asp:TextBox ID="NameBox" runat="server"></asp:TextBox> <br />
+            <p>Email: </p> <asp:TextBox ID="MailBox" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="SignupBtn" runat="server" Text="Sign up" OnClick="SignupBtn_Click" />
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ OurWebsite/OurWebsite/Forms/SignupForm.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OurWebsite.Forms
+{
+    public partial class SignupForm : System.Web.UI.Page
+    {
+        private string _path = AppDomain.CurrentDomain.BaseDirectory;
+        private string _fileName = @"\database.txt";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void SignupBtn_Click(object sender, EventArgs e)
+        {
+
+            //Using the streamwriter to create the file and write to it...
+            using (System.IO.StreamWriter sw = System.IO.File.CreateText(_path + _fileName))
+            {
+
+                sw.WriteLine(NameBox.Text + ":" + MailBox.Text);
+            }
+            
+            Response.Redirect("~/Personas/Us");
+        }
+
+    }
+}
