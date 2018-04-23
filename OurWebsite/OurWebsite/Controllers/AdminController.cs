cef9f70caf4ba6895ec3724044ce6eb8991feeaa
kevan14 <genger-man@hotmail.com>
2018-04-23 14:41:22 +0200

    Picture upload works

---
+++ OurWebsite/OurWebsite/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurWebsite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult UploadPicture()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePersona()
+        {
+            return View();
+        }
+    }
+}
---
+++ OurWebsite/OurWebsite/Forms/UploadForm.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OurWebsite.Forms
+{
+    public partial class UploadForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Photos/") + fileName);
+                Response.Redirect("~/Photos/Gallery");
+            }
+        }
+    }
+}
