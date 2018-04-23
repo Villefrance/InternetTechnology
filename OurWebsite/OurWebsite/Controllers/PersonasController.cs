fc2ffeed23b879a769fd284489b1e49ee787ff6b
kevan14 <genger-man@hotmail.com>
2018-04-23 14:27:25 +0200

    Personas are read from persistence file system

--- OurWebsite/OurWebsite/Controllers/PersonasController.cs
+++ OurWebsite/OurWebsite/Controllers/PersonasController.cs
@@ -9,6 +9,9 @@ namespace OurWebsite.Controllers
 {
     public class PersonasController : Controller
     {
+        private string _path = AppDomain.CurrentDomain.BaseDirectory;
+        private string _fileName = @"\personadatabase.txt";
+
         // GET: Personas/index
         public ActionResult Index()
         {
@@ -17,11 +20,32 @@ namespace OurWebsite.Controllers
 
         public ActionResult Us()
         {
-            IList<Persona> personas = new List<Persona>(); 
-            Persona emil = new Persona() { Education = "Software Engineering", Age = 23, Description = "Hello there, I like to party and make smooth tender love with my wife.", Name = "Emil" };
-            Persona kennet = new Persona() { Education = "Software Engineering", Age = 25, Description = "Hello there, I like to shit and EAT all day long.", Name = "Kennet" };
-            personas.Add(emil);
-            personas.Add(kennet);
+            IList<Persona> personas = new List<Persona>();
+
+            //Need to check if the file exists otherwise an exception is thrown.
+            if (System.IO.File.Exists(_path + _fileName))
+            {
+
+                using (System.IO.StreamReader sr = System.IO.File.OpenText(_path + _fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //The format of a stored spersona is a line like: value1:value2:...  this is the reasoning for splitting by ":" 
+                        string[] protoypePersona = line.Split(':');
+
+                        personas.Add(new Persona() { Education = protoypePersona[0], Age = Int16.Parse(protoypePersona[1]), Description = protoypePersona[2], Name = protoypePersona[3] });
+
+                    }
+                }
+
+            }
+
+
+            //Persona emil = new Persona() { Education = "Software Engineering", Age = 23, Description = "Hello there, I like to party and make smooth tender love with my wife.", Name = "Emil" };
+            //Persona kennet = new Persona() { Education = "Software Engineering", Age = 25, Description = "Hello there, I like to shit and EAT all day long.", Name = "Kennet" };
+            //personas.Add(emil);
+            //personas.Add(kennet);
             return View(personas);
         }
     }
